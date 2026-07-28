0e210de454fc4aa31b7552988ef4acf0a9b9c344
association-ansfal <bendaoud.said@gmail.com>
2026-07-28 14:37:05 +0100

    Delete api directory

--- api/storage.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
-const STORAGE_BUCKET = 'media';
-
-export { supabase, STORAGE_BUCKET };
